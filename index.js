0b8881bb58daba7627fcbddfb7afe42fdef85652
David Garcia <dgarciabravo13@gmail.com>
2020-10-06 13:44:36 +0200

    conect with mongo Atlas

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("desde index.js 1");
+const express = require("express");
+const conectarDB = require("./config/db");
+
+//crear el servidor
+const app = express();
+
+//conectar a la bbdd
+conectarDB();
+
+//puerto de app
+const PORT = process.env.PORT || 4000;
+
+//arrancar la app
+app.listen(PORT, () =>{
+  console.log(`el servidor esta funcionando en el puerto ${PORT}`);
+})
