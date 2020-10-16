d3bbdb75eb16dd31b704d61788deb03c98d9d6a2
David Garcia <dgarciabravo13@gmail.com>
2020-10-16 13:52:47 +0200

    checking if the user and password are correct

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,53 @@
 const Usuario = require("../models/Usuario");
 const bcryptjs = require("bcryptjs");
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
-exports.autenticarUsuario = async (req,res) => {
+exports.autenticarUsuario = async (req, res) => {
+  //revisar si existen errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    res.status(400).json({ errores: errores.array() });
+  }
 
-    //revisar si existen errores
-    const errores = validationResult(req);
-    if(!errores.isEmpty()){
-      res.status(400).json({errores:errores.array()});
+  //extraer email y password
+  const { email, password } = req.body;
+
+  try {
+    //Revisar que sea un usuario registrado
+    let usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({ msg: "El usuario no existe" });
+    }
+    //Revisar el password
+    const passCorrecto = await bcryptjs.compare(password, usuario.password);
+    if (!passCorrecto) {
+      return res.status(400).json({ msg: "Password incorrecto" });
     }
 
-}
+    //Si todo es correcto crear y firmar el jwt
 
+    const payload = {
+      usuario: {
+        id: usuario.id,
+      },
+    };
+
+    //firmar el jwt
+
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600,
+      },
+      (error, token) => {
+        if (error) throw error;
+        //mensaje de confirmación
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
