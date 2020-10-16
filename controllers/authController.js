d5cb243994ef36c661487ca941e9cea2d521de1a
David Garcia <dgarciabravo13@gmail.com>
2020-10-16 12:25:44 +0200

    add authentication user

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const {validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.autenticarUsuario = async (req,res) => {
+
+    //revisar si existen errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+      res.status(400).json({errores:errores.array()});
+    }
+
+}
+
