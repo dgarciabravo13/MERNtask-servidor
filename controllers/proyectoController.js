13485ceed34eb6e19e4f887cba8a7187d3990f6e
David Garcia <dgarciabravo13@gmail.com>
2020-10-19 20:02:42 +0200

    update project

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -26,12 +26,58 @@ exports.crearProyecto = async (req, res) => {
 
 //Obtiene todos los proyectos del usuario actual
 
-exports.obtenerProyectos = async (req,res) => {
+exports.obtenerProyectos = async (req, res) => {
   try {
-    const proyectos = await Proyecto.find({creador:req.usuario.id});
+    const proyectos = await Proyecto.find({ creador: req.usuario.id });
     res.json(proyectos);
   } catch (error) {
     console.log(error);
     res.status(500).send("Hubo un error");
   }
-}
+};
+
+//Actualiza un proyecto
+
+exports.actualizarProyecto = async (req, res) => {
+  //revisar si existen errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    res.status(400).json({ errores: errores.array() });
+  }
+
+  //extraer la informacion del proyecto
+  const { nombre } = req.body;
+  const nuevoProyecto = {};
+
+  if (nombre) {
+    nuevoProyecto.nombre = nombre;
+  }
+
+  try {
+    //revisar el id
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    //si existe el proyecto o no
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+
+    //verificar el creador del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    //actualizar
+    proyecto = await Proyecto.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: nuevoProyecto },
+      { new: true }
+    );
+
+    res.json({ proyecto });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error en el servidor");
+  }
+};
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -17,10 +17,20 @@ router.post(
   proyectoController.crearProyecto
 );
 
+//Obtener todos los proyectos
 router.get(
   "/",
   auth,
   proyectoController.obtenerProyectos
+);
+
+//Actualizar un proyecto
+router.put("/:id",
+  auth,
+  [
+    check("nombre", "El nombre el obligatorio").not().isEmpty()
+  ],
+  proyectoController.actualizarProyecto
 );
 
 module.exports = router;
