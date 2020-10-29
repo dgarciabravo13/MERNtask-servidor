e1ad5171480b1457b2440d391b8da7a0571af0a1
David Garcia <dgarciabravo13@gmail.com>
2020-10-29 14:39:04 +0100

    inserting tasks and the project to which they belong

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -8,4 +8,29 @@ exports.crearTarea = async (req, res) =>{
     if (!errores.isEmpty()) {
       res.status(400).json({ errores: errores.array() });
     }
+
+    //Extraer el proyecto
+    const {proyecto} = req.body;
+    //Comprobar si existe
+    try {
+
+      const existeProyecto = await Proyecto.findById(proyecto);
+      if(!existeProyecto){
+        return res.status(404).json({msg:"Proyecto no encontrado"});
+      }
+
+      //Revisar si el proyecto actual pertenece al usuario autenticado
+      if (existeProyecto.creador.toString() !== req.usuario.id) {
+        return res.status(401).json({ msg: "No autorizado" });
+      }
+
+      //Creamos la tarea
+      const tarea = new Tarea(req.body);
+      await tarea.save();
+      res.json({tarea});
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Hubo un error");
+    }
 };
