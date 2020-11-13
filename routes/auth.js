6daabb2a699f0164fdf83185c87fec5326264ac0
David Garcia <dgarciabravo13@gmail.com>
2020-11-13 13:34:52 +0100

    validate log in

--- routes/auth.js
+++ routes/auth.js
@@ -10,13 +10,6 @@ const auth = require("../middleware/auth");
 
 router.post(
   "/",
-
-  [
-    check("email", "Agrega un email valido").isEmail(),
-    check("password", "El password debe ser minimo 6 caracteres").isLength({
-      min: 6,
-    }),
-  ],
   authController.autenticarUsuario
 );
 
