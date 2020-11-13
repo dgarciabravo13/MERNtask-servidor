209efd97d86db889cc84a2419e7e234305f44275
David Garcia <dgarciabravo13@gmail.com>
2020-11-13 12:57:50 +0100

    sending token by headers

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
+
+//Iniciar sesion
+//api/auth
 
 router.post(
   "/",
@@ -15,5 +19,10 @@ router.post(
   ],
   authController.autenticarUsuario
 );
+
+router.get("/",
+  auth,
+  authController.usuarioAutenticado
+)
 
 module.exports = router;
